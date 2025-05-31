b506031229604e0330c7043e97497f03b72a3cb3
anga83 <7267739+anga83@users.noreply.github.com>
2025-05-31 17:42:27 +0200

    Add files via upload

---
+++ Avoid_Reddit_Translate.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Reddit Language Parameter Remover
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Removes /?tl= language parameters from Reddit URLs (this usually happens when opened via Google or other search engines) to avoid Reddit post from being auto-translated and reloads automatically.
+// @author       https://github.com/anga83
+// @match        *://www.reddit.com/*
+// @match        *://reddit.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    const currentUrl = window.location.href;
+    const hasLanguageParam = currentUrl.includes('/?tl=');
+    
+    if (hasLanguageParam) {
+        const cleanUrl = currentUrl.replace(/\/\?tl=[^&?]*(&|$)/, '/');
+        
+        if (cleanUrl !== currentUrl) {
+            window.location.replace(cleanUrl);
+        }
+    }
+})();
